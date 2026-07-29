19029b95b09b56accb1f851d8238c54ecf9ba760
dong-junn <djl020326@gmail.com>
2026-07-29 16:12:10 +0900

    docker-entrypoint.sh: docker restart시 UID&PID를 배번 동작하도록 하는 것이 아닌, 최초 실행시에만 동작하도록 수정

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -33,6 +33,12 @@ esac
 CURRENT_UID="$(id -u satisfactory)"
 CURRENT_GID="$(id -g satisfactory)"
 
+IDENTITY_CHANGED=0
+
+if [ "$CURRENT_UID" != "$PUID" ] || [ "$CURRENT_GID" != "$PGID" ]; then
+    IDENTITY_CHANGED=1
+fi
+
 if [ "$CURRENT_GID" != "$PGID" ]; then
     groupmod --non-unique --gid "$PGID" satisfactory
 fi
@@ -55,11 +61,31 @@ chown satisfactory:satisfactory \
     /home/satisfactory \
     /home/satisfactory/.config
 
-chown -R satisfactory:satisfactory \
-    /home/satisfactory/.fex-emu \
-    /home/satisfactory/steamcmd \
-    /home/satisfactory/.config/Epic \
-    /home/satisfactory/server-file
+ensure_tree_owner() {
+    TARGET_PATH="$1"
+    MISMATCHED_PATH="$(
+        find "$TARGET_PATH" \
+            \( ! -uid "$PUID" -o ! -gid "$PGID" \) \
+            -print \
+            -quit
+    )"
+
+    if [ -n "$MISMATCHED_PATH" ]; then
+        echo "Correcting ownership: $TARGET_PATH"
+        chown -R satisfactory:satisfactory "$TARGET_PATH"
+    fi
+}
+
+if [ "$IDENTITY_CHANGED" -eq 1 ]; then
+    chown -R satisfactory:satisfactory \
+        /home/satisfactory/.fex-emu \
+        /home/satisfactory/steamcmd \
+        /home/satisfactory/.config/Epic \
+        /home/satisfactory/server-file
+else
+    ensure_tree_owner /home/satisfactory/.config/Epic
+    ensure_tree_owner /home/satisfactory/server-file
+fi
 
 exec setpriv \
     --reuid=satisfactory \
